0b95af280ceae6851a3031c3fda5230befb1ccde
Kourichoufleur <gabriel.fabre05@gmail.com>
2025-11-19 20:08:24 +0100

    creation de groupe privée (client)

--- src/Client.java
+++ src/Client.java
@@ -157,41 +157,71 @@ public class Client {
 
 	}
 
-	public void envoie_message(String message, String pseudo)
+	public void creer_groupe(String nom, ArrayList<String> liste_ami) {
+		try {
+			String mes_amis = "";
+			for (String ami : liste_ami) {
+				mes_amis += ami + "|";
+			}
+			this.envoie_message("groupe_etape_1", nom, mes_amis);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void envoie_message(String intitule, String pseudo, String message)
 			throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
-		for (Contact mon_ami : contacts) {// recherche de mon ami
-			if (mon_ami.pseudo.equals(pseudo)) {// si j'ai un ami
-				String host = PARAMETRE.host;
-				int port = PARAMETRE.port;
-				// tentative de conexion au serveur
-				try (Socket socket = new Socket(host, port);
-						PrintWriter out = new PrintWriter(socket.getOutputStream(), true);) {
+		if (pseudo.equals("server")) {
+			String host = PARAMETRE.host;
+			int port = PARAMETRE.port;
+			try (Socket socket = new Socket(host, port);
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);) {
 
-					System.out.println("Connecté au serveur sur " + host + ":" + port);
-					System.out.println("Tape un message (ou 'bye' pour quitter) :");
+				System.out.println("Connecté au serveur sur " + host + ":" + port);
+				out.println(intitule + "|server|" + message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			for (Contact mon_ami : contacts) {// recherche de mon ami
+				if (mon_ami.pseudo.equals(pseudo)) {// si j'ai un ami
+					String host = PARAMETRE.host;
+					int port = PARAMETRE.port;
+					// tentative de conexion au serveur
+					try (Socket socket = new Socket(host, port);
+							PrintWriter out = new PrintWriter(socket.getOutputStream(), true);) {
 
-					if (message.equalsIgnoreCase("bye")) {
-						out.println("bye||");
-						int[] tab = { 0 };
-						int bidule = tab[1];// euh non mais tkt
-					}
-					try {
-						Cipher cipher = Cipher.getInstance("AES");
-						cipher.init(Cipher.ENCRYPT_MODE, mon_ami.la_clef);
-						byte[] encrypte = cipher.doFinal(message.getBytes());
-						out.println(
-								"message" + "|" + mon_ami.pseudo + "|" + new String(encrypte, StandardCharsets.UTF_8));
-					} catch (IllegalBlockSizeException | BadPaddingException e) {
-						// TODO Auto-generated catch block
+						System.out.println("Connecté au serveur sur " + host + ":" + port);
+						System.out.println("Tape un message (ou 'bye' pour quitter) :");
+
+						if (message.equalsIgnoreCase("bye")) {
+							out.println("bye||");
+							int[] tab = { 0 };
+							int bidule = tab[1];// euh non mais tkt
+						}
+						try {
+							Cipher cipher = Cipher.getInstance("AES");
+							cipher.init(Cipher.ENCRYPT_MODE, mon_ami.la_clef);
+							byte[] encrypte = cipher.doFinal(message.getBytes());
+							if (intitule.equals(null)) {
+								intitule = "message";
+							}
+							out.println(intitule + "|" + mon_ami.pseudo + "|"
+									+ new String(encrypte, StandardCharsets.UTF_8));
+						} catch (IllegalBlockSizeException | BadPaddingException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					} catch (IOException e) {
 						e.printStackTrace();
+					} catch (IndexOutOfBoundsException e) {
+						System.out.println("Je suis parti");
 					}
 
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (IndexOutOfBoundsException e) {
-					System.out.println("Je suis parti");
 				}
-
 			}
 		}
 
@@ -201,36 +231,51 @@ public class Client {
 		try {
 			boolean cond = true;
 			String[] decoupe = message.split("|", 3);
-			if (decoupe[1] == "server") {
-				// gerer les message server
-			} else {
-				switch (decoupe[0]) {
-				case "message":
-					for (Contact mon_ami : contacts) {
-						if (mon_ami.pseudo.equals(decoupe[1])) {
-							Cipher cipher = Cipher.getInstance("AES");
-							cipher.init(Cipher.DECRYPT_MODE, mon_ami.la_clef);
-							byte[] valeur = decoupe[2].getBytes(StandardCharsets.UTF_8);
-							System.out.println(cipher.doFinal(valeur));
-							cond = false;
-							break;
-
-						}
-					}
-					if (cond) {
-						// créer le contact
+			switch (decoupe[0]) {
+			case "message":
+				for (Contact mon_ami : contacts) {
+					if (mon_ami.pseudo.equals(decoupe[1])) {
+						// Decrypte le message et l'affiche
+						Cipher cipher = Cipher.getInstance("AES");
+						cipher.init(Cipher.DECRYPT_MODE, mon_ami.la_clef);
+						byte[] valeur = decoupe[2].getBytes(StandardCharsets.UTF_8);
+						System.out.println(mon_ami.pseudo + ":\n" + cipher.doFinal(valeur));
+						cond = false;
+						break;
 
 					}
-					break;
-				case "demande":
-					byte[] valeur = decoupe[2].getBytes(StandardCharsets.UTF_8);
-					Cipher cipher = Cipher.getInstance("RSA");
-					cipher.init(Cipher.DECRYPT_MODE, this.cle_prive);
-					Contact nouvel_ami = new Contact(decoupe[1], new SecretKeySpec(cipher.doFinal(valeur), "AES"));
-					contacts.add(nouvel_ami);
-					break;
+				}
+				if (cond) {
+					// céer le contact
+					System.out.println("ne doit jamais arriver");
+				}
+				break;
+			case "groupe_etape_2":
+				// genere la clef aes l'encrypte avec chaque clef publique et envoie un message
+				// avec groupe_etape_3 le nom du groupe et les clefs aes séparé par des pipes
+				KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+				keyGen.init(128);
+				SecretKey aesKey = keyGen.generateKey();
+				contacts.add(new Contact(decoupe[1], aesKey));
+				String liste_clefs = "";
+				Cipher cipher = Cipher.getInstance("RSA");
+				for (String clef_public : decoupe[2].split("|")) {
+					cipher.init(Cipher.ENCRYPT_MODE,
+							new SecretKeySpec(clef_public.getBytes(StandardCharsets.UTF_8), "RSA"));
+					liste_clefs += cipher.doFinal(aesKey.getEncoded()) + "|";
 
 				}
+				this.envoie_message("groupe_etape_3", decoupe[1], liste_clefs);
+
+			case "groupe_etape_final":
+				// création du groupe
+				byte[] valeur = decoupe[2].getBytes(StandardCharsets.UTF_8);
+				Cipher cipher1 = Cipher.getInstance("RSA");
+				cipher1.init(Cipher.DECRYPT_MODE, this.cle_prive);
+				Contact nouvel_ami = new Contact(decoupe[1], new SecretKeySpec(cipher1.doFinal(valeur), "AES"));
+				contacts.add(nouvel_ami);
+				break;
+
 			}
 
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException
