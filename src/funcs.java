87532c5066884e9fb838a66a0917cdd03065b7ad
Kourichoufleur <gabriel.fabre05@gmail.com>
2025-12-07 22:15:28 +0100

    Update funcs.java

--- src/funcs.java
+++ src/funcs.java
@@ -12,18 +12,23 @@ import javax.crypto.spec.SecretKeySpec;
 /**Une classe contenant des methodes statiques bien utiles
  **/
 public class funcs {
-	
-	
+
+    /**DECRYPTER la clef aes grace à sa clef privée rsa quand on rejoint un groupe ou le serveur
+     * @param tableau un string de la forme [..., ..., ...]
+     **/
 	static SecretKey RSA_DECRYPT(String tableau, PrivateKey clef_privee) throws Exception {
 		Cipher cipher = Cipher.getInstance("RSA");
 		cipher.init(Cipher.DECRYPT_MODE, clef_privee);
 		return new SecretKeySpec(cipher.doFinal(stringToByte_Mais_pff(tableau)), "AES");
 	}
 
-
+    /**ENCRYPTER la clef aes grace à la clef publique rsa du client quand il rejoint un groupe ou le serveur
+     * @param tableau un string de la forme [..., ..., ...]
+     **/
 	static String RSA_ENCRYPT(SecretKey clef_aes, String tableau) throws Exception {
 		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 		X509EncodedKeySpec spec = new X509EncodedKeySpec(Base64.getDecoder().decode(tableau));
+        // permte d'éviter les problemes de chiffrement en base 64
 		KeyFactory factory = KeyFactory.getInstance("RSA");
 		PublicKey clef_publique = factory.generatePublic(spec);
 		cipher.init(Cipher.ENCRYPT_MODE, clef_publique);
